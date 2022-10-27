683f62cbc334c727e58be4cd58588d534e95044d
itlemon <lemon_jiang@aliyun.com>
2022-10-28 00:48:47 +0800

    add: 数组的二分查找

--- src/main/java/cn/codingguide/leetcode/array/execises/No704BinarySearch.java
+++ src/main/java/cn/codingguide/leetcode/array/execises/No704BinarySearch.java
@@ -58,4 +58,65 @@ public class No704BinarySearch {
         return -1;
     }
 
+    /**
+     * 主动找的思路：左闭右开
+     *
+     * @param nums 待查找的数组
+     * @param target 目标元素
+     * @return 下标或者-1
+     */
+    public int search3(int[] nums, int target) {
+        // 定义三个变量
+        int left = 0;
+        int right = nums.length;
+        int mid;
+
+        // 跳出循环的条件是 left == right，当left = right - 1的时候，是最后一次循环，此时待查找区间只剩下一个元素
+        // 如果最后一个元素不等于目标元素，说明数组nums里面不含目标元素，直接返回 -1
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                // 右边是单开，所以只能是mid，不是mid - 1
+                right = mid;
+            }
+        }
+
+        // 如果不存在目标元素，直接返回-1
+        return -1;
+    }
+
+    /**
+     * 主动找的思路：左闭右闭
+     *
+     * @param nums 待查找的数组
+     * @param target 目标元素
+     * @return 下标或者-1
+     */
+    public int search4(int[] nums, int target) {
+        // 定义三个变量
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        // 跳出循环的条件是 left == right，当left = right - 1的时候，是最后一次循环，此时待查找区间只剩下两个元素
+        // 检查完 right - 1 的元素后，就跳出了，少检查了一个 right的元素，也就是这种判断条件在循环体中漏掉了一个元素
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // 最后还要检查循环体没有检查的元素才能得出正确结果
+        return nums[left] == target ? left : -1;
+    }
+
 }
