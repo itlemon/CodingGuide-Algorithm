4d9a4787db0a693bbccad5eac06e29651f865854
itlemon <lemon_jiang@aliyun.com>
2022-10-29 15:38:33 +0800

    add: 数组的二分查找

--- src/main/java/cn/codingguide/leetcode/array/execises/No704BinarySearch.java
+++ src/main/java/cn/codingguide/leetcode/array/execises/No704BinarySearch.java
@@ -119,4 +119,31 @@ public class No704BinarySearch {
         return nums[left] == target ? left : -1;
     }
 
+    /**
+     * 排除法的思路：左闭右闭
+     *
+     * @param nums 待查找的数组
+     * @param target 目标元素
+     * @return 下标或者-1
+     */
+    public int search5(int[] nums, int target) {
+        // 定义三个变量
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        // 跳出循环的条件是 left == right，当 left = right - 1 的时候，是最后一次循环，此时待查找区间只剩下两个元素
+        // 检查完 right - 1 的元素后，就跳出了，少检查了一个 right的元素，也就是这种判断条件在循环体中漏掉了一个元素
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        // 最后还要检查循环体没有检查的元素才能得出正确结果
+        return nums[left] == target ? left : -1;
+    }
 }
